051619150ef1313b5be17f1979c6a95e704ab753
mayank <mayankkhajure@gmail.com>
2022-09-14 15:10:54 +0530

    cart committed last

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,6 +3,7 @@ class Food < ApplicationRecord
   # Associations
   belongs_to :restaurant
   has_many :cart_items
+  has_many_attached :images
 
   #VALIDATIONS
   validates :name, :price, :quantity, presence: true
